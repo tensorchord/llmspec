f87a3d3149896c62e07334aaa3bae74c273114e7
Keming <kemingyang@tensorchord.ai>
2023-05-16 23:34:48 +0800

    feat: support basic OpenAI chat completion & emb API req/resp (#13)
    * feat: support basic OpenAI chat completion & emb API req/resp
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * add glm req converter
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    ---------
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- llmspec/llmspec.py
+++ llmspec/llmspec.py
@@ -1,6 +1,9 @@
 import json
-from typing import Union, Optional
+from typing import Union, Optional, Dict, List
+from datetime import datetime
 from enum import Enum
+
+import msgspec
 
 
 class Role(Enum):
@@ -15,29 +18,94 @@ class Role(Enum):
         return [item.value for item in cls]
 
 
-class ChatMessage:
+class ChatMessage(msgspec.Struct):
     """Unified chat message interface."""
 
-    def __init__(
-        self, content: str, role: Role = Role.USER, name: Optional[str] = None
-    ) -> None:
-        self.content = content
-        self.role = role
-        self.name = name
+    content: str
+    role: Role = Role.USER
+    name: str = ""
 
-        self.validate()
 
-    def validate(self):
-        if not self.content or not isinstance(self.content, str):
-            raise ValueError("content should be str with length > 0")
-        if self.role not in Role.values():
-            raise ValueError(f"role should be one of the {Role.values()}")
+class CompletionRequest(msgspec.Struct, kw_only=True):
+    suffix: Optional[str] = None
+    max_tokens: int = 16
+    temperature: float = 1.0
+    top_p: float = 1.0
+    n: int = 1
+    stream: bool = False
+    stop: Optional[str] = None
+    presence_penalty: float = 0.0
+    frequency_penalty: float = 0.0
+    logit_bias: Optional[Dict] = None
+    user: str = ""
 
-    def dict(self):
-        msg = {"role": self.role, "content": self.content}
-        if self.name:
-            msg["name"] = self.name
-        return msg
+    @classmethod
+    def from_bytes(cls, buf: bytes):
+        return msgspec.json.decode(buf, type=cls)
+
+
+class PromptCompletionRequest(CompletionRequest):
+    model: str
+    prompt: str = "<|endoftext|>"
+    echo: bool = False
+    logprobs: Optional[int] = None
+    best_of: int = 1
+
+
+class ChatCompletionRequest(CompletionRequest):
+    model: str
+    messages: List[ChatMessage]
+
+    def to_model(self, model: str = "ChatGLM"):
+        if model.lower() == "chatglm":
+            return dict(
+                prompt=self.messages[-1].content,
+                history=[msg.content for msg in self.messages[:-1]],
+                max_length=self.max_tokens,
+                top_p=self.top_p,
+                temperature=self.temperature,
+            )
+        # return dict by default
+        return msgspec.structs.asdict(self)
+
+
+class ChatChoice(msgspec.Struct):
+    message: ChatMessage
+    index: int = 0
+    finish_reason: str = "stop"
+
+
+class TokenUsage(msgspec.Struct):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+
+
+class CompletionResponse(msgspec.Struct):
+    id: str
+    object: str
+    created: datetime
+    choices: List[ChatChoice]
+    usage: TokenUsage
+
+
+class EmbeddingRequest(msgspec.Struct):
+    model: str
+    input: Union[str, List[str]]
+    user: str = ""
+
+
+class EmbeddingData(msgspec.Struct):
+    embedding: List[float]
+    index: int
+    object: str = "embedding"
+
+
+class EmbeddingResponse(msgspec.Struct):
+    data: EmbeddingData
+    model: str
+    usage: TokenUsage
+    object: str = "list"
 
 
 class LLMSpec:
