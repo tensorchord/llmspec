f53849384a3e7d2b84d92fd493ca8b293cad4448
Keming <kemingyang@tensorchord.ai>
2023-06-07 12:06:42 +0800

    feat: add prompt template (#39)
    * feat: add prompt template
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * add test
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * format
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    ---------
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ format:
 	@black ${PY_SOURCE}
 	@ruff check --fix .
 
+test:
+	@pytest -vv -s
+
 # install cspell by `npm i -g cspell`
 cspell-collect-unknown-words:
 	@cspell --words-only --unique '{*.py{,nb},{!({,.}venv)/**/*.{html,py,js,ts,css,md,yaml,yml,json,txt,code-snippets,ipynb,Rmd,R},.github/**/*.{md,yaml,yml}}}' | LC_ALL='C' sort --ignore-case > project-words.txt
---
+++ tests/prompt_test.py
@@ -0,0 +1,48 @@
+import pytest
+
+from llmspec.llmspec import ChatCompletionRequest, ChatMessage, Role
+
+
+@pytest.fixture
+def messages():
+    return [
+        ChatMessage(role=Role.USER, content="Who are you?"),
+        ChatMessage(role=Role.ASSISTANT, content="I'm a bot."),
+        ChatMessage(role=Role.USER, content="Do you like English?"),
+    ]
+
+
+@pytest.mark.parametrize(
+    "model,messages,expected",
+    [
+        ("thudm/chatglm", None, (
+            "[Round 0]\n"
+            "问：Who are you?\n"
+            "答：I'm a bot.\n"
+            "[Round 1]\n"
+            "问：Do you like English?\n"
+        )),
+        ("lmsys/vicuna", None, (
+            "USER: Who are you?\n"
+            "ASSISTANT: I'm a bot.</s>\n"
+            "USER: Do you like English?\n"
+            "ASSISTANT: "
+        )),
+        ("lmsys/fastchat", None, (
+            "USER: Who are you?\n### "
+            "ASSISTANT: I'm a bot.\n### "
+            "USER: Do you like English?\n### "
+            "ASSISTANT: "
+        )),
+        ("decapoda-research/llama", None, (
+            "USER: Who are you?\n"
+            "ASSISTANT: I'm a bot.\n"
+            "USER: Do you like English?\n"
+            "ASSISTANT: "
+        ))
+    ],
+    indirect=["messages"],
+)
+def test_chat_prompt_generation(model, messages, expected):
+    chat = ChatCompletionRequest(model=model, messages=messages)
+    assert chat.get_prompt(model) == expected
